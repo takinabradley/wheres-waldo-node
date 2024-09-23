a14aa3074f8f8b12dc80acdc98eb93a74e158c8c
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2024-09-23 12:39:25 -0400

    introduce client-side routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: "This route doesn't exist!",
+    children: [
+     { path: "/message",
+      element: "React router"}
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
