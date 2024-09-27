572e911335deb0e5f33b6edc4c77e9033f5bc4ee
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2024-09-27 15:23:46 -0400

    update character schema

--- server/models/Character.js
+++ server/models/Character.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose"
 const characterSchema = mongoose.Schema({
   name: String,
   pos: [Number, Number],
+  imgUrl: "string",
 })
 
 const Character = mongoose.model("Character", characterSchema)
