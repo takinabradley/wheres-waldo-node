0a01af89d4187dd916f18ab1ccfee9a9d0553356
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2024-09-27 21:57:42 -0400

    create methods of getting and selecting characters, and starting and ending a game

--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ import ViteExpress from "vite-express"
 import process from "node:process"
 import session from "express-session"
 
+import Character from "./models/Character.js"
+
 const appDebug = debug("wheres-waldo:app")
 const viteConfigFile = path.resolve(import.meta.dirname, "../vite.config.js")
 
@@ -23,6 +25,7 @@ mongoose
   .catch(() => appDebug("mongoose failed to connect:\n"))
 
 const app = express()
+
 app.use(
   session({
     name: "wheres-waldo",
@@ -41,7 +44,60 @@ app.use(
   })
 )
 
-app.get("/message", (_, res) => res.send("Hello from express!!!"))
+app.use(express.json())
+
+app.get("/animals.jpg", (req, res, next) => {
+  // start the game as soon as the image is loaded
+  // reset any session data to restart the game
+  session.startTime = Date.now()
+  session.endTime = undefined
+  session.foundCharacters = []
+  next()
+})
+
+const apiRouter = express.Router()
+
+apiRouter.post("/seek-and-find/characters", async (req, res, next) => {
+  // if endTime has been calculated already, just send that. Game is over.
+  if (session.endTime)
+    return res.json({ endTime: session.endTime - session.startTime })
+
+  const allCharacters = await Character.find()
+
+  const character = await Character.findOne({
+    "pos.0": { $gte: req.body?.percentX - 5, $lte: req.body?.percentX + 5 },
+    "pos.1": { $gte: req.body?.percentY - 5, $lte: req.body?.percentY + 5 },
+  })
+
+  // if a character is found, add it to the list of found characters this session
+  if (character && !session.foundCharacters.includes(character._id.toString()))
+    session.foundCharacters.push(character._id.toString())
+
+  // if all characters are found, set the end time
+  if (
+    allCharacters.every((character) =>
+      session.foundCharacters.includes(character._id.toString())
+    )
+  ) {
+    session.endTime = Date.now()
+  }
+
+  // send back the character and the endtime
+  // The endtime and last character are generally found at the same time
+  return res.json({
+    character,
+    endTime: session.endTime ? session.endTime - session.startTime : null,
+  })
+})
+
+apiRouter.get("/seek-and-find/characters", async (req, res, next) => {
+  const characters = await Character.find({}, "name imgUrl")
+  res.json(characters)
+})
+
+app.use("/api", apiRouter)
 
 ViteExpress.config({ viteConfigFile })
-ViteExpress.listen(app, 3000, () => console.log("Server is listening..."))
+ViteExpress.listen(app, 3000, () =>
+  console.log("Server listening at http://localhost:3000")
+)
